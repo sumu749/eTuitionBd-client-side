ad56294c33a8a736ee3759dcef72f744b91700c7
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-05-23 00:02:09 +0600

    folder structure created

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardLayout = () => {
+    return (
+        <div>
+            <h1>THIS IS DASHBOARD LAYOUT</h1>
+        </div>
+    );
+};
+
+export default DashboardLayout;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1>THIS IS MAIN LAYOUT</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
