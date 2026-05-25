b21cdbde24a9261d687dd425416ccfcb15deea0b
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-05-25 22:05:54 +0600

    my tuition page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import AuthProvider from "./providers/AuthProvider.jsx";
-import { RouterProvider } from "react-router";
-import { router } from "./routes/Router.jsx";
-import { Toaster } from "react-hot-toast";
 import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { router } from "./routes/Router";
+import AuthProvider from "./providers/AuthProvider";
+
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AuthProvider>
-            <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <RouterProvider router={router} />
 
-            <Toaster position="top-right" reverseOrder={false} />
-        </AuthProvider>
+                <Toaster position="top-right" />
+            </AuthProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 );
--- src/pages/StudentDashboard/MyTuitions.jsx
+++ src/pages/StudentDashboard/MyTuitions.jsx
@@ -1,7 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../hooks/useAuth";
+import axiosSecure from "../../api/axiosSecure";
+
 const MyTuitions = () => {
+    const { user } = useAuth();
+
+    const { data = [], isLoading } = useQuery({
+        queryKey: ["my-tuitions", user?.email],
+
+        enabled: !!user?.email,
+
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/my-tuitions/${user.email}`);
+
+            return res.data;
+        },
+    });
+
+    if (isLoading) {
+        return <span className="loading loading-spinner loading-lg"></span>;
+    }
+
     return (
         <div>
-            <h1>My Tuitions</h1>
+            <h1 className="text-3xl font-bold mb-8">My Tuitions</h1>
+
+            <div className="overflow-x-auto ">
+                <table className="table ">
+                    <thead>
+                        <tr className="bg-slate-800 text-slate-400">
+                            <th>Subject</th>
+                            <th>Class</th>
+                            <th>Location</th>
+                            <th>Budget</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {data.map((tuition) => (
+                            <tr key={tuition._id}>
+                                <td>{tuition.subject}</td>
+
+                                <td>{tuition.classLevel}</td>
+
+                                <td>{tuition.location}</td>
+
+                                <td>৳{tuition.budget}</td>
+
+                                <td>{tuition.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
